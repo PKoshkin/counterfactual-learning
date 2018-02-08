536899c23acada3c49715aa24c8809bda5f8b3b7
onaga1958 <alex--zabelin@mail.ru>
2018-02-08 13:55:25 +0300

    add batches in xgboost_model's fit, now no segfault, but process is killed on 26k obj

--- code/cpp/xgboost_model.cpp
+++ code/cpp/xgboost_model.cpp
@@ -1,34 +1,47 @@
 #include "xgboost_model.h"
 
+#include <algorithm>
 
-XGBoostModel::XGBoostModel(uint16_t iteration_number) : iteration_number(iteration_number) {}
+
+XGBoostModel::XGBoostModel(uint16_t iteration_number, uint16_t batch_size)
+    : iteration_number(iteration_number), batch_size(batch_size) {}
 
 
 void XGBoostModel::fit(const Matrix& features, const std::vector<double>& scores) {
+    std::cout << "\nStart preparing data" << std::endl;
     DMatrixHandle train_data;
-    const int rows = features.size();
     const int cols = features[0].size();
-    float train_features[rows][cols];
-    float train_labels[rows];
+    float train_features[batch_size][cols] = {0};
+    float train_labels[batch_size] = {0};
 
-    for (int row = 0; row < rows; ++row) {
-        for (int col = 0; col < cols; ++col)
-            train_features[row][col] = static_cast<float>(features[row][col]);
-        train_labels[row] = static_cast<float>(scores[row]);
-    }
-
-    XGDMatrixCreateFromMat((float *) train_features, rows, cols, -1, &train_data);
-    XGDMatrixSetFloatInfo(train_data, "label", train_labels, rows);
+    XGDMatrixCreateFromMat((float *) train_features, batch_size, cols, -1, &train_data);
+    XGDMatrixSetFloatInfo(train_data, "label", train_labels, batch_size);
 
     XGBoosterCreate(&train_data, 1, &booster);
     XGBoosterSetParam(booster, "objective", "reg:linear");
     XGBoosterSetParam(booster, "eta", "0.1");
     XGBoosterSetParam(booster, "silent", "1");
 
-    for (int iteration = 0; iteration < iteration_number; ++iteration)
-	XGBoosterUpdateOneIter(booster, iteration, train_data);
+    std::cout << "\nStart training" << std::endl;
+
+    for (int iteration = 0; iteration < iteration_number; ++iteration) {
+        for (int batch_start_ind = 0; batch_start_ind < features.size(); batch_start_ind += batch_size) {
+            int rows = std::min(batch_size, static_cast<uint16_t>(features.size() - batch_start_ind));
+            for (int row = 0; row < rows; ++row) {
+                for (int col = 0; col < cols; ++col)
+                    train_features[row][col] = static_cast<float>(features[batch_start_ind + row][col]);
+                train_labels[row] = static_cast<float>(scores[batch_start_ind + row]);
+            }
+
+            XGDMatrixCreateFromMat((float *) train_features, rows, cols, -1, &train_data);
+            XGDMatrixSetFloatInfo(train_data, "label", train_labels, rows);
+
+            XGBoosterUpdateOneIter(booster, iteration, train_data);
+        }
+    }
 
     XGDMatrixFree(train_data);
+    std::cout << "\nEnd training" << std::endl;
 }
 
 
--- code/cpp/xgboost_model.h
+++ code/cpp/xgboost_model.h
@@ -9,8 +9,9 @@ class XGBoostModel : public BaseModel {
 public:
     virtual double predict(const std::vector<double>& features) const;
     virtual void fit(const Matrix& features, const std::vector<double>& scores);
-    XGBoostModel(uint16_t iteration_number);
+    XGBoostModel(uint16_t iteration_number, uint16_t batch_size = 1000);
 private:
     BoosterHandle booster;
     uint16_t iteration_number;
+    uint16_t batch_size;
 };
