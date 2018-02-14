8da222197a5b23da4d405e2539b1e29fab2ee531
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-02-14 16:50:59 +0300

    Can not fit linear model

--- code/python/networks/eleven_regressions_model.py
+++ code/python/networks/eleven_regressions_model.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-from one_position_model import OnePositionModel
-
 class ElevenRegressionsModel:
-    def __init__(self, num_features):
+    def __init__(self, num_features, OnePositionModel):
         self.NONE_POSITION = 11
         self.POSITIONS = list(range(1, self.NONE_POSITION))
         self.models = [
@@ -12,31 +10,18 @@ class ElevenRegressionsModel:
             for position in self.POSITIONS
         ]
 
-    def teach(self, train_pool, test_pool, verbose=True, iterations=1000):
+    def teach(self, train_pool, verbose=True, iterations=1000):
         train_pools = train_pool.split_by_position()
-        test_pools = test_pool.split_by_position()
         for i, model in enumerate(self.models):
-            train_log, test_log = model.teach(
-                train_pools[i].features, train_pools[i].regression_prediction,
-                test_pools[i].features, test_pools[i].regression_prediction,
+            train_log = model.teach(
+                train_pools[i].features, train_pools[i].regression_prediction, iterations
             )
-            counter = 0
-            best_test_loss = np.min(test_log)
-            while best_test_loss > 1:
-                train_log, test_log = model.teach(
-                    train_pools[i].features, train_pools[i].regression_prediction,
-                    test_pools[i].features, test_pools[i].regression_prediction,
-                )
-                best_test_loss = np.min(test_log)
-                counter += 1
-                if counter > 20:
-                    break
             if verbose:
-                print('position {}: {} ({} try)'.format(i + 1, np.min(test_log), counter))
+                print(' position loss {}: {}'.format(i + 1, train_log[-1]))
 
-    def predict_positions(self, test_features, verbose=True):
+    def predict_positions(self, test_pool, verbose=True):
         prediction = []
-        for features in test_features:
+        for i, features in enumerate(test_pool.features):
             max_score = -100
             best_position = -100
             scores_log = []
@@ -47,9 +32,11 @@ class ElevenRegressionsModel:
                     max_score = new_score
                     best_position = position
             if verbose:
-                print(scores_log, np.max(scores_log))
+                if (np.argmax(scores_log) + 1 == test_pool.positions[i]) and (test_pool.targets[i] != 0):
+                    print(np.round(scores_log, 2), np.round(np.max(scores_log), 2), np.round(test_pool.targets[i], 2))
             if max_score > 0:
                 prediction.append(best_position)
             else:
                 prediction.append(self.NONE_POSITION)
+
         return prediction
