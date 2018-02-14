c8ffa739f63c64aa5ca839a630a9c4d3aaae9ad7
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-02-14 21:03:15 +0300

    Can not teach Keras Layes

--- code/python/networks/one_regression_conv_model.py
+++ code/python/networks/one_regression_conv_model.py
@@ -1,47 +1,58 @@
 import tensorflow as tf
-from keras.layers import Conv1D
+from keras.layers import Conv1D, Dense, Flatten 
 from keras import backend as Keras
 import numpy as np
 
 class OneRegressionConvModel:
     def __init__(self, num_features):
         self.num_features = num_features
-        self.POSITIONS = list(range(11))
+        self.NONE_POSITION = 11
+        self.POSITIONS = list(range(1, self.NONE_POSITION))
+
+        self.sess = tf.Session()
+        Keras.set_session(self.sess)
+
         self.input_features = tf.placeholder('float32', shape=(None, num_features, 1))
         self.input_prediction = tf.placeholder('float32', shape=(None, 1))
+
         conv = Conv1D(
             filters=8,
             kernel_size=100,
             activation='relu'
         )
-        self.output_prediction = conv(self.input_features)
+        dense = Dense(1, activation='relu')
+
+        self.output_prediction = dense(Flatten()(conv(self.input_features)))
+
         self.loss = (
             tf.reduce_mean((self.input_prediction - self.output_prediction) ** 2)
         )
-        self.optimizer = tf.train.AdamOptimizer().minimize(
-            self.loss, var_list=conv.weights
-        )
+        self.optimizer = tf.train.AdamOptimizer().minimize(self.loss)#, var_list=(conv.weights + dense.weights))
 
-        self.sess = tf.Session()
-        Keras.set_session(self.sess)
         self.sess.run(tf.global_variables_initializer())
 
     def teach(self, train_features, train_prediction,
-              verbose=True, iterations=100):
-        for i in range(iterations):
-            self.sess.run(
-                self.optimizer, {
-                    self.input_features: self.reshape_features(train_features),
-                    self.input_prediction: train_prediction
-                }
-            )
-            if verbose:
-                train_loss = self.get_regression_loss(train_features, train_prediction)[0]
-                print(
-                    "train loss: {}".format(
-                        train_loss
+              verbose=True, iterations=5, batch_size=128):
+        with self.sess.as_default():
+            for i in range(iterations):
+                objects_number = len(train_features)
+                batch_start_index = 0
+                while batch_start_index + batch_size <= objects_number:
+                    batch_slice = slice(batch_start_index, batch_start_index + batch_size)
+                    self.sess.run(
+                        self.optimizer, {
+                            self.input_features: self.reshape_features(train_features)[batch_slice],
+                            self.input_prediction: train_prediction[batch_slice]
+                        }
                     )
-                )
+                    if verbose:
+                        train_loss = self.get_regression_loss(train_features[batch_slice], train_prediction[batch_slice])
+                        print(
+                            "train loss: {}".format(
+                                train_loss
+                            )
+                        )
+                    batch_start_index += batch_size
 
     def reshape_features(self, features):
         return np.reshape(features, (len(features), self.num_features, 1))
@@ -69,11 +80,12 @@ class OneRegressionConvModel:
                         0 if possible_position != position else 1
                         for possible_position in self.POSITIONS
                     ]
-                ])
+                ])[0]
                 scores_log.append(new_score)
                 if new_score > max_score:
                     max_score = new_score
                     best_position = position
+            scores_log = np.array(scores_log)
             if verbose:
                 if (np.argmax(scores_log) + 1 == test_pool.positions[i]) and (test_pool.targets[i] != 0):
                     print(np.round(scores_log, 2), np.round(np.max(scores_log), 2), np.round(test_pool.targets[i], 2))
