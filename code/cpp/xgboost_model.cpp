6f721fabaaaab5e2af7c2c75458e282ffd031907
onaga1958 <alex--zabelin@mail.ru>
2018-02-20 22:24:06 +0300

    add RAII handels

--- code/cpp/xgboost_model.cpp
+++ code/cpp/xgboost_model.cpp
@@ -5,17 +5,35 @@
 #include <exception>
 
 
+void XGBoosterRAIIHandle::reset() {if (booster) XGBoosterFree(booster);}
+BoosterHandle* XGBoosterRAIIHandle::get_ptr() {return &booster;}
+BoosterHandle XGBoosterRAIIHandle::get() const {return booster;}
+XGBoosterRAIIHandle::XGBoosterRAIIHandle(BoosterHandle booster) : booster(booster) {}
+XGBoosterRAIIHandle::~XGBoosterRAIIHandle() {if (booster) XGBoosterFree(booster);}
+
+
+class DMatrixRAIIHandle {
+private:
+    DMatrixHandle data;
+public:
+    DMatrixHandle* get() {return &data;}
+    DMatrixRAIIHandle(DMatrixHandle data = nullptr) : data(data) {}
+    ~DMatrixRAIIHandle() {if (data) XGDMatrixFree(data);}
+};
+
+
 XGBoostModel::XGBoostModel(uint16_t iteration_number, const BoosterParams& booster_params)
-    : iteration_number(iteration_number), booster_params(booster_params), booster(nullptr) {}
+    : iteration_number(iteration_number), booster_params(booster_params), booster() {}
 
 
 void XGBoostModel::fit(const Matrix& features, const std::vector<double>& scores) {
     std::shared_ptr<float> train_features;
     std::shared_ptr<float> train_labels;
-    DMatrixHandle train_data;
 
     while (true) {
         try {
+            booster.reset();
+            DMatrixRAIIHandle train_data;
             int rows = features.size();
             int cols = features[0].size();
 
@@ -23,41 +41,31 @@ void XGBoostModel::fit(const Matrix& features, const std::vector<double>& scores
             train_labels = std::shared_ptr<float>(new float[rows]);
 
             for (int row = 0; row < rows; ++row) {
-                for (int col = 0; col < cols; ++col)
-                    train_features.get()[row * cols + col] = static_cast<float>(features[row][col]);
+                for (int col = 0; col < cols; ++col) {
+                    int index = row * cols + col;
+                    train_features.get()[index] = static_cast<float>(features[row][col]);
+                }
                 train_labels.get()[row] = static_cast<float>(scores[row]);
             }
 
-            XGDMatrixCreateFromMat(train_features.get(), rows, cols, -1, &train_data);
-            XGDMatrixSetFloatInfo(train_data, "label", train_labels.get(), rows);
+            XGDMatrixCreateFromMat(train_features.get(), rows, cols, -1, train_data.get());
+            XGDMatrixSetFloatInfo(train_data.get(), "label", train_labels.get(), rows);
 
-            if (!booster)
-                XGBoosterFree(booster);
-            XGBoosterCreate(&train_data, 1, &booster);
-            XGBoosterSetParam(booster, "eta", "0.1");
-            XGBoosterSetParam(booster, "max_depth", "3");
-            XGBoosterSetParam(booster, "base_score", "0");
-            XGBoosterSetParam(booster, "silent", "1");
+            XGBoosterCreate(train_data.get(), 1, booster.get_ptr());
+            XGBoosterSetParam(booster.get(), "eta", "0.1");
+            XGBoosterSetParam(booster.get(), "max_depth", "3");
+            XGBoosterSetParam(booster.get(), "base_score", "0");
+            XGBoosterSetParam(booster.get(), "silent", "1");
 
             for (const auto& param: booster_params)
-                XGBoosterSetParam(booster, param.first.c_str(), param.second.c_str());
+                XGBoosterSetParam(booster.get(), param.first.c_str(), param.second.c_str());
 
             for (int iteration = 0; iteration < iteration_number; ++iteration)
-                XGBoosterUpdateOneIter(booster, iteration, train_data);
+                XGBoosterUpdateOneIter(booster.get(), iteration, *train_data.get());
 
-            XGDMatrixFree(train_data);
             return;
         }
-        catch (std::bad_alloc err) {
-            try {
-                XGDMatrixFree(train_data);
-                if (!booster) {
-                    XGBoosterFree(booster);
-                    booster = nullptr;
-                }
-            }
-            catch (std::exception _err) {}
-        }
+        catch (std::bad_alloc err) {}
     }
 }
 
@@ -75,7 +83,7 @@ double XGBoostModel::predict(const std::vector<double>& features) const {
 
     bst_ulong out_len;
     const float * prediction;
-    XGBoosterPredict(booster, test_data, 0, 0, &out_len, &prediction);
+    XGBoosterPredict(booster.get(), test_data, 0, 0, &out_len, &prediction);
 
     XGDMatrixFree(test_data);
     return prediction[0];
@@ -95,7 +103,7 @@ std::vector<double> XGBoostModel::predict_proba(const std::vector<double>& featu
 
     bst_ulong out_len;
     const float * prediction;
-    XGBoosterPredict(booster, test_data, 0, 0, &out_len, &prediction);
+    XGBoosterPredict(booster.get(), test_data, 0, 0, &out_len, &prediction);
 
     XGDMatrixFree(test_data);
 
@@ -103,9 +111,4 @@ std::vector<double> XGBoostModel::predict_proba(const std::vector<double>& featu
     for (int i = 0; i < out_len; ++i)
         result[i] = prediction[i];
     return result;
-}
-
-
-XGBoostModel::~XGBoostModel() {
-    XGBoosterFree(booster);
 }
--- code/cpp/xgboost_model.h
+++ code/cpp/xgboost_model.h
@@ -8,15 +8,26 @@
 typedef std::vector<std::pair<std::string, std::string>> BoosterParams;
 
 
+class XGBoosterRAIIHandle {
+private:
+    BoosterHandle booster;
+public:
+    BoosterHandle* get_ptr();
+    BoosterHandle get() const;
+    void reset();
+    XGBoosterRAIIHandle(BoosterHandle booster = nullptr);
+    ~XGBoosterRAIIHandle();
+};
+
+
 class XGBoostModel : public BaseModel {
 public:
     virtual double predict(const std::vector<double>& features) const;
     std::vector<double> predict_proba(const std::vector<double>& features) const;
     virtual void fit(const Matrix& features, const std::vector<double>& scores);
     XGBoostModel(uint16_t iteration_number, const BoosterParams& booster_params = {});
-    ~XGBoostModel();
 private:
     BoosterParams booster_params;
-    BoosterHandle booster;
+    XGBoosterRAIIHandle booster;
     uint16_t iteration_number;
 };
