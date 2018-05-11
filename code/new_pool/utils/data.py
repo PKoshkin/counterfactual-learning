15a78b89707e2be3e647e00d48e2f8de433c96ac
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-05-11 09:11:53 +0300

    Files calculating

---
+++ code/new_pool/utils/data.py
@@ -0,0 +1,37 @@
+from json import loads as json_from_string
+from datetime import datetime
+
+
+def make_catboost_line(json):
+    result_list = [json['target'], json['pos'], json['ts']] + json['factors']
+    return "\t".join(map(str, result_list))
+
+
+def make_catboost_file(json_filename, result_filename):
+    with open(json_filename) as json_handler, open(result_filename, 'w') as result_handler:
+        for line in json_handler:
+            json = json_from_string(line.strip())
+            print(make_catboost_line(json), file=result_handler)
+
+
+def check_sorted(json_filename):
+    with open(json_filename) as json_handler:
+        ts = -1
+        for line in json_handler:
+            json = json_from_string(line.strip())
+            new_ts = json['ts']
+            if ts > new_ts:
+                return False
+            else:
+                ts = new_ts
+        return True
+
+
+def get_days(catboost_file):
+    dates = set()
+    with open(catboost_file) as handler:
+        for line in handler:
+            ts = int(line.split()[2])
+            date = datetime.fromtimestamp(ts).date()
+            dates.add(date)
+    return dates
