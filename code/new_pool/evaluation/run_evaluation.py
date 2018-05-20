7e8f22e6ef11b140ad979202126975c2472dfa3a
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-05-20 17:15:40 +0300

    Classification argmax evaluation

--- code/new_pool/evaluation/run_evaluation.py
+++ code/new_pool/evaluation/run_evaluation.py
@@ -1,9 +1,14 @@
 import argparse
 from evaluate_regression import evaluate_regression
+from evaluate_classification import evaluate_classification
+
+
+class ArgumentException(Exception):
+    pass
 
 
 def main():
-    types = ["evaluate_regression"]
+    types = ["evaluate_regression", "evaluate_classification"]
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_folder", type=str, required=True)
     parser.add_argument("--predictions_folder", type=str, required=True)
@@ -12,6 +17,10 @@ def main():
     args = parser.parse_args()
     if args.type == "evaluate_regression":
         evaluate_regression(args.predictions_folder, args.data_folder, args.out_folder)
+    elif args.type == "evaluate_classification":
+        evaluate_classification(args.predictions_folder, args.data_folder, args.out_folder)
+    else:
+        raise ArgumentException("Wrong type \"{}\"".format(args.type))
 
 
 if __name__ == "__main__":
