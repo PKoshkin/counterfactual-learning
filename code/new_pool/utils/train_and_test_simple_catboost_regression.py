00a27795ab6404d7fe8aea5514de42f561e99a5e
pkoshkin <koshkin.pasha@gmail.com>
2018-06-07 15:12:09 +0300

    deleting unnuued files

--- code/new_pool/utils/train_and_test_simple_catboost_regression.py
+++
@@ -1,6 +0,0 @@
-from catboost import CatBoostRegressor
-from run_train_and_test_simple_regression import run
-
-
-if __name__ == "__main__":
-    run(lambda verbose: CatBoostRegressor(verbose=verbose))
--- code/new_pool/utils/train_and_test_simple_xgboost_regression.py
+++
@@ -1,6 +0,0 @@
-import xgboost as xgb
-from run_train_and_test_simple_regression import run
-
-
-if __name__ == "__main__":
-    run(lambda verbose: xgb.XGBRegressor(silent=not verbose))
