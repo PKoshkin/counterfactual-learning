9e1895a3c3e4e9efa9f5110916df22ef42c0da9d
pkoshkin <koshkin.pasha@gmail.com>
2018-06-15 00:52:39 +0300

    evaluation logs

--- code/python/evaluation/evaluate.py
+++ code/python/evaluation/evaluate.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import numpy as np
+import datetime
 import sys
 import os
 
@@ -14,20 +15,24 @@ def evaluate(predictions_folder, data_folder, out_folder, positions_by_predictio
         os.path.join(predictions_folder, filename)
         for filename in os.listdir(predictions_folder)
     ])
+    print("\"{}\": predictions_filenames: {}".format(str(datetime.datetime.now()), predictions_filenames))
     # prediction filename is like "train_i_test_j" where i and j are int days numbers (hope their len is 1)
     days = sorted([int(filename[-1]) for filename in predictions_filenames])
+    print("\"{}\": will evaluate days: {}".format(str(datetime.datetime.now()), days))
     data_filenames = [
         os.path.join(data_folder, "day_{}.json".format(day))
         for day in days
     ]
     with open(os.path.join(out_folder, "metrics.txt"), "w") as handler:
         for predictions_filename, data_filename in zip(predictions_filenames, data_filenames):
+            print("\"{}\": predict on file \"{}\"".format(str(datetime.datetime.now()), predictions_filename))
             predictions = np.load(predictions_filename)
             predicted_positions = positions_by_predictions(predictions)
             probs = get_from_pool(pool_iterator(data_filename), "p", float)
             target_positions = get_from_pool(pool_iterator(data_filename), "pos", int)
             targets = get_from_pool(pool_iterator(data_filename), "target", int)
 
+            print("\"{}\": calculating metric".format(str(datetime.datetime.now())))
             metric = calculate_metric(predicted_positions, target_positions, targets, probs)
             counter = len(target_positions[target_positions == predicted_positions])
             print(metric, counter, file=handler)
